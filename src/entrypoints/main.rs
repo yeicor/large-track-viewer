9bec035ae18dfb171234ceb4ce15406d4f87cdfe
Yeicor <4929005+Yeicor@users.noreply.github.com>
2025-10-21 16:38:07 +0200

    Refactor build workflow and move index.html to src/entrypoints

--- src/entrypoints/main.rs
+++ src/entrypoints/main.rs
@@ -1,5 +1,3 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
 // === Entry point for desktop ===
 #[allow(dead_code)]
 #[cfg(not(target_arch = "wasm32"))]
